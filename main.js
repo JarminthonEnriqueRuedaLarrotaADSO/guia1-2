717e77b2a4bdc23ae0311d5675920a7cb8e0e678
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-21 09:01:39 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 21. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
-    // por favor utilizar funciones matemáticas de Python para ello
-    // d = rc((x2-x1)^2 + (y2-y1)^2)
-    let valX = Number(prompt("Ingrese el valor de x:  ", ));
-    let valY = Number(prompt("Ingrese el valor de y:  ", ));
-    let valRC = Number(prompt("Ingrese el valor de R:  ", ));
-    let solucion= valRC*((valX*(2)-valX*(1))**2 + (valY*(2)-valY*(1))**2)
-    console.log(`%cFormula (${'d = rc((x2-x1)^2 + (y2-y1)^2)'} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`La solucion de la formula es : "${solucion}"`);
+    // 22.Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de
+    // descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total
+    // de la compra.
+    let compra1 = Number(prompt("Ingrese el valor de su compra 1:  ", ));
+    let compra2 = Number(prompt("Ingrese el valor de su compra 2:  ", ));
+    let compra3 = Number(prompt("Ingrese el valor de su compra 3:  ", ));
+    let compra4 = Number(prompt("Ingrese el valor de su compra 4:  ", ));
+    let compra5 = Number(prompt("Ingrese el valor de su compra 5:  ", ));
+    let descuentoC1 = compra1*0.05 
+    let descuentoC2 = compra2*0.05
+    let descuentoC4 = compra3*0.02
+    let descuentoC5 = compra4*0.02
+    let apliDescuento1 = compra1-descuentoC1
+    let apliDescuento2 = compra2-descuentoC2
+    let apliDescuento4 = compra4-descuentoC4
+    let apliDescuento5 = compra5-descuentoC5
+
+
     
-    let num1 = Number(prompt("Ingrese el numero 1", 2));
-    let num2 = Number(prompt("Ingrese el numero 2", 2));
-
-    console.log(`La suma es ${num1} + ${num2} = ${num1 + num2}`);
-    console.log(`La resta es ${num1} - ${num2} = ${num1 - num2}`);
-    console.log(`La multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
-    console.log(`La division es ${num1} / ${num2} = ${num1 / num2}`);
-
-
-
-
-
-
-
+    console.log(`%cFormula (${"Se multiplica el valor de la compra por el descuento y se resta ese valor a la compra original..."}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`El precio del producto 3 es de: "${compra3}"`);
+    console.log(`El precio del producto 1 con descuento es de: ${apliDescuento1}`)
+    console.log(`El precio del producto 2 con descuento es de: ${apliDescuento2}`)
+    console.log(`El precio del producto 4 con descuento es de: ${apliDescuento4}`)
+    console.log(`El precio del producto 5 con descuento es de: ${apliDescuento5}`)    
 })
