7e212e30b111b4fa4b843b1d5c04f3e63f2ef3ee
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-21 10:34:03 -0500

    Ejercicio 6

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 25.En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
-    // de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
-    // nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
-    // hacer el bizcocho es
-    let harina = 100
-    let grcacao = 10
-    let grcacao1 = Number(prompt('introduce los gramos de cacao: ',20))
-    let solucion = (harina*grcacao1)/grcacao
+    // 26. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg
+    // transporta ?
+    let cajas = 25
+    let undCaja = 748 
+    let solucion = (cajas*undCaja)
     
 
-    console.log(`%cFormula (${"solucion = (harina*grcacao1)/grcacao"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`la cantidad de grampos de harina para hacer bizcocho es:   "${solucion}"`);
+    console.log(`%cFormula (${"solucion = (cajas*undCaja)"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`la cantidad de kg que transporta el camion es:"${solucion}"`);
     
 })
