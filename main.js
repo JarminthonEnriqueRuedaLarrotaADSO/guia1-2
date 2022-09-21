7bdb6d4bb6422cfaf770b4a4df3d126467bf4f92
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-21 10:49:00 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 26. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg
-    // transporta ?
-    let cajas = 25
-    let undCaja = 748 
-    let solucion = (cajas*undCaja)
-    
+    // 27. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
+    // 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
+    // Lorena?
+    let Martin = 2/3
+    let Jairo = 1/4 
+    let solucion = (Martin+Jairo)
+    let lorena = (1/1)-solucion
 
-    console.log(`%cFormula (${"solucion = (cajas*undCaja)"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`la cantidad de kg que transporta el camion es:"${solucion}"`);
+    console.log(`%cFormula (${"solucion = (1/1)-solucion(solucion es la suma de martin y jairo)"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`la cantidad que le pretenece a lorena es:"${lorena}"`);
     
 })
