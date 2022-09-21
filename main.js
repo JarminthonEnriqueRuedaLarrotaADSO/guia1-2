727475f21510af2801ab61387fa819b6b110bfec
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-21 10:29:54 -0500

    Ejercicio 5

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 24. Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por
-// $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le
-// sobran $91000. ¿cuánto dinero tenía?
-    let sobrante = 91000
-    let JuegosdeLlaves = 11500*5
-    let Bomba = 1168000
-    let cajaPerno = 87000*3
-    let sumaCompra = JuegosdeLlaves+Bomba+cajaPerno+sobrante
+    // 25.En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
+    // de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
+    // nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
+    // hacer el bizcocho es
+    let harina = 100
+    let grcacao = 10
+    let grcacao1 = Number(prompt('introduce los gramos de cacao: ',20))
+    let solucion = (harina*grcacao1)/grcacao
+    
 
-    console.log(`%cFormula (${"se calcula el precio de la compra y se le suma el restante inicial.."}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`El dinero que traia es:   "${sumaCompra}"`);
+    console.log(`%cFormula (${"solucion = (harina*grcacao1)/grcacao"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`la cantidad de grampos de harina para hacer bizcocho es:   "${solucion}"`);
     
 })
