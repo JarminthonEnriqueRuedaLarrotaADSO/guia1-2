c9f4d1e0b2d78ec1000e8b7848127417414a8718
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-21 11:20:01 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 29. Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
-    // pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
-    // que representen el algoritmo para solucionar este problema.
-    // Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula
-    // sostenida y la edad del entrevistado.
-    let nombreTrabj = prompt('ingrese el nombre del trabajador: ')
-    let añoNacimiento = Number(prompt('ingrese el año en el que nacio:  ',))
-    let añoActual = Number(prompt('ingrese el año en el que se encuentra:  ', )) 
-    let solucion = (añoActual-añoNacimiento)
+    // 30. . Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase
+    // invertida.
+
+    let palabra = prompt('ingrese el nombre del trabajador: ')
+    console.log(palabra)
+    let palabraInvertida = palabra.split('').reverse().join('')
+
     
-    console.log(`%cFormula (${"solucion=(año actual-Edad del trabajador)"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`El nombre del trabajador es "${nombreTrabj}" y su edad es : ${solucion}`);
+    console.log(`%cFormula (${"palabraInvertida = palabra.split('').reverse().join('')"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`"${palabraInvertida}"`);
 
 })
