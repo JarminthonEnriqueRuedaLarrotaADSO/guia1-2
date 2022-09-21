fb944b6b1c459e694d92ab4018ac8cb3aeaf86bb
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-21 10:56:21 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 27. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
-    // 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
-    // Lorena?
-    let Martin = 2/3
-    let Jairo = 1/4 
-    let solucion = (Martin+Jairo)
-    let lorena = (1/1)-solucion
-
-    console.log(`%cFormula (${"solucion = (1/1)-solucion(solucion es la suma de martin y jairo)"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`la cantidad que le pretenece a lorena es:"${lorena}"`);
+    // 28 .Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura. Considere que
+    // se cobra por m2 y realice el código que representen el algoritmo que le permita ir generando
+    // presupuestos para cada cliente.
+    let m_cdrado = Number(prompt('introduce metros cuadrados para pintar ',))
+    let presupuesto_mcuadrado = Number(prompt('introduce valor metro cuadrado segun material ', )) 
+    let solucion = (m_cdrado*presupuesto_mcuadrado)
     
+    console.log(`%cFormula (${"solucion = (m_cdrado*presupuesto_mcuadrado)"}} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`El total de dinero a pagar por metro cuadrado pintado es:"${solucion}"`);
+
 })
